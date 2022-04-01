9a5dd31a11478264acea816aedaa978483cb35e5
jean <jeansantos@id.uff.br>
2022-04-01 11:03:46 -0300

    requisito 10 enviado

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,14 @@ localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it('Teste se saveCartItems quando recebe o argumento <ol><li>Item</li></ol> o método localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Teste se saveCartItems quando recebe dois argumentos, "cartItems"e "<ol><li>Item</li></ol>" respectivamente o método localStorage é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
+
