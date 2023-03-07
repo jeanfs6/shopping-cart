cd8818f75dfd1cf59ca7ae19609978b6caddd58d
Jean Fonseca <94874689+jeanfs6@users.noreply.github.com>
2023-03-07 19:25:12 -0300

    Delete helpers directory

--- helpers/getSavedCartItems.js
+++
@@ -1,8 +0,0 @@
-const getSavedCartItems = async () => {
-  const getCartItem = localStorage.getItem('cartItem');
-  return getCartItem;
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = getSavedCartItems;
-}
--- helpers/saveCartItems.js
+++
@@ -1,7 +0,0 @@
-const saveCartItems = async (cartItemsAdd) => {
-  localStorage.setItem('cartItem', cartItemsAdd); // nome da chave  e o valor
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
-}
